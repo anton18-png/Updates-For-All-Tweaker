ad04eed8220e51d831470e160a76327244510d17
Solomon Chik <86909941+anton18-png@users.noreply.github.com>
2025-07-01 22:11:14 +0300

    Add files via upload

---
+++ user.py
@@ -0,0 +1,233 @@
+USER_THEMES = {
+    "cyberpunk": {
+        "type": "dark",
+        "colors": {
+            "primary": "#ea38b8",
+            "secondary": "#ea38b8",
+            "success": "#3af180",
+            "info": "#1da2f2",
+            "warning": "#ffbd05",
+            "danger": "#ea38b8",
+            "light": "#44d7e8",
+            "dark": "#170229",
+            "bg": "#190831",
+            "fg": "#32fbe2",
+            "selectbg": "#ea38b8",
+            "selectfg": "#32fbe2",
+            "border": "#ea38b8",
+            "inputfg": "#32fbe2",
+            "inputbg": "#30115e",
+            "active": "#17082E"
+        }
+    },
+    "hacker": {
+        "type": "dark",
+        "colors": {
+            "primary": "#3af180",
+            "secondary": "#3af180",
+            "success": "#3af180",
+            "info": "#3af180",
+            "warning": "#3af180",
+            "danger": "#3af180",
+            "light": "#3af180",
+            "dark": "#3af180",
+            "bg": "#222222",
+            "fg": "#3af180",
+            "selectbg": "#3af180",
+            "selectfg": "#3af180",
+            "border": "#222222",
+            "inputfg": "#3af180",
+            "inputbg": "#2f2f2f",
+            "active": "#3af180"
+        }
+    },
+    "green": {
+        "type": "light",
+        "colors": {
+            "primary": "#56cc9d",
+            "secondary": "#56cc9d",
+            "success": "#56cc9d",
+            "info": "#56cc9d",
+            "warning": "#56cc9d",
+            "danger": "#56cc9d",
+            "light": "#56cc9d",
+            "dark": "#56cc9d",
+            "bg": "#f8f9fa",
+            "fg": "#5a5a5a",
+            "selectbg": "#56cc9d",
+            "selectfg": "#f8f9fa",
+            "border": "#56cc9d",
+            "inputfg": "#696969",
+            "inputbg": "#fff",
+            "active": "#56cc9d"
+        }
+    },
+    "palenight": {
+        "type": "dark",
+        "colors": {
+            "primary": "#89ddff",
+            "secondary": "#444444",
+            "success": "#50c878",
+            "info": "#89ddff",
+            "warning": "#f39c12",
+            "danger": "#ff5874",
+            "light": "#adb5bd",
+            "dark": "#212432",
+            "bg": "#292d3e",
+            "fg": "#cccccc",
+            "selectbg": "#6d4fa7",
+            "selectfg": "#cccccc",
+            "border": "#222222",
+            "inputfg": "#cccccc",
+            "inputbg": "#25293a",
+            "active": "#1F1F1F"
+        }
+    },
+    "boosterx": {
+        "type": "dark",
+        "colors": {
+            "primary": "#7930ff",
+            "secondary": "#d93b3b",
+            "success": "#31ff83",
+            "info": "#2e5cff",
+            "warning": "#ffbd05",
+            "danger": "#d93b3b",
+            "light": "#2e5cff",
+            "dark": "#0a0f21",
+            "bg": "#1b2352",
+            "fg": "#ffffff",
+            "selectbg": "#7930ff",
+            "selectfg": "#ffffff",
+            "border": "#7930ff",
+            "inputfg": "#bfb6cd",
+            "inputbg": "#0a0f21",
+            "active": "#7930ff"
+        }
+    },
+    "boosterxvapor": {
+        "type": "dark",
+        "colors": {
+            "primary": "#7930ff",
+            "secondary": "#d93b3b",
+            "success": "#31ff83",
+            "info": "#2e5cff",
+            "warning": "#ffbd05",
+            "danger": "#d93b3b",
+            "light": "#2e5cff",
+            "dark": "#0a0f21",
+            "bg": "#190831",
+            "fg": "#ffffff",
+            "selectbg": "#7930ff",
+            "selectfg": "#ffffff",
+            "border": "#7930ff",
+            "inputfg": "#bfb6cd",
+            "inputbg": "#0a0f21",
+            "active": "#7930ff"
+        }
+    },
+    "wincry": {
+        "type": "dark",
+        "colors": {
+            "primary": "#ff1744",
+            "secondary": "#ff1744",
+            "success": "#31ff83",
+            "info": "#2e5cff",
+            "warning": "#ffbd05",
+            "danger": "#ff1744",
+            "light": "#2e5cff",
+            "dark": "#0a0f21",
+            "bg": "#0a0a0a",
+            "fg": "#ffffff",
+            "selectbg": "#ff1744",
+            "selectfg": "#ffffff",
+            "border": "#ffffff",
+            "inputfg": "#bfb6cd",
+            "inputbg": "#0a0f21",
+            "active": "#ffffff"
+        }
+    },
+    "ruslanchik": {
+        "type": "dark",
+        "colors": {
+            "primary": "#ff1744",
+            "secondary": "#ff1744",
+            "success": "#31ff83",
+            "info": "#ffbd05",
+            "warning": "#ffbd05",
+            "danger": "#ff1744",
+            "light": "#ffbd05",
+            "dark": "#0a0f21",
+            "bg": "#0a0a0a",
+            "fg": "#ffffff",
+            "selectbg": "#ff1744",
+            "selectfg": "#ffffff",
+            "border": "#ffffff",
+            "inputfg": "#bfb6cd",
+            "inputbg": "#0a0f21",
+            "active": "#ffffff"
+        }
+    },
+    "revi_os": {
+        "type": "dark",
+        "colors": {
+            "primary": "#ff1744",
+            "secondary": "#ff1744",
+            "success": "#31ff83",
+            "info": "#2e5cff",
+            "warning": "#ffbd05",
+            "danger": "#ff1744",
+            "light": "#2e5cff",
+            "dark": "#0a0f21",
+            "bg": "#0a0a0a",
+            "fg": "#ffffff",
+            "selectbg": "#ff1744",
+            "selectfg": "#ffffff",
+            "border": "#ffffff",
+            "inputfg": "#bfb6cd",
+            "inputbg": "#0a0f21",
+            "active": "#ffffff"
+        }
+    },
+    "wincry_warning": {
+        "type": "dark",
+        "colors": {
+            "primary": "#ff1744",
+            "secondary": "#ff1744",
+            "success": "#31ff83",
+            "info": "#ffbd05",
+            "warning": "#ffbd05",
+            "danger": "#ff1744",
+            "light": "#ffbd05",
+            "dark": "#0a0a0a",
+            "bg": "#0a0a0a",
+            "fg": "#ffbd05",
+            "selectbg": "#ff1744",
+            "selectfg": "#ffbd05",
+            "border": "#ffbd05",
+            "inputfg": "#ffbd05",
+            "inputbg": "#0a0a0a",
+            "active": "#ffbd05"
+        }
+    },
+    "wincry_full_warning": {
+        "type": "dark",
+        "colors": {
+            "primary": "#ff1744",
+            "secondary": "#ff1744",
+            "success": "#ffbd05",
+            "info": "#ffbd05",
+            "warning": "#ffbd05",
+            "danger": "#ff1744",
+            "light": "#ffbd05",
+            "dark": "#0a0a0a",
+            "bg": "#0a0a0a",
+            "fg": "#ffbd05",
+            "selectbg": "#ff1744",
+            "selectfg": "#ffbd05",
+            "border": "#ffbd05",
+            "inputfg": "#ffbd05",
+            "inputbg": "#0a0a0a",
+            "active": "#ffbd05"
+        }
+    }
+}
